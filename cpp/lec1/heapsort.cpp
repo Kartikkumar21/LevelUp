bc0b4c4fe4ce03506af40eda7e6ac81bda56d0cd
Kartikkumar21 <60642198+Kartikkumar21@users.noreply.github.com>
2021-01-04 12:48:42 +0530

    update

--- cpp/lec1/heapsort.cpp
+++ cpp/lec1/heapsort.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-bool compareTo(vector<int> &arr, bool isMax, int a, int b)
+bool compareTo(int a, int b, vector<int> &arr, bool isMax)
 {
     if (isMax)
         return arr[a] > arr[b];
@@ -11,47 +11,43 @@ bool compareTo(vector<int> &arr, bool isMax, int a, int b)
         return arr[b] > arr[a];
 }
 
-void downHeapify(vector<int> &arr,int pi, bool isMax,int n)
+void downHeapify(int pi, int n, vector<int> &arr, bool isMax)
 {
-    int maxidx = pi;
+    int maxIdx = pi;
     int lci = 2 * pi + 1;
     int rci = 2 * pi + 2;
 
-    if (lci <= n && compareTo(arr,isMax,lci, maxidx))
-        maxidx = lci;
-    if (rci <= n && compareTo(arr,isMax,rci, maxidx))
-        maxidx = rci;
+    if (lci <= n && compareTo(lci, maxIdx, arr, isMax))
+        maxIdx = lci;
+    if (rci <= n && compareTo(rci, maxIdx, arr, isMax))
+        maxIdx = rci;
 
-    if (maxidx != pi)
+    if (maxIdx != pi)
     {
-        swap(arr[pi], arr[maxidx]);
-        downHeapify(arr,maxidx,isMax,n);
+        swap(arr[maxIdx], arr[pi]);
+        downHeapify(maxIdx, n, arr, isMax);
     }
 }
 
-void solve()
+void heapSort()
 {
     vector<int> arr = {10, 20, 30, -2, -3, -4, 5, 6, 7, 8, 9, 22, 11, 13};
-    int n = arr.size();
-    bool isMax = true;
-    
-    for (int i = n - 1; i >= 0; i--)
+    bool isMax = false;
+    int n = arr.size() - 1;
+    for (int i = n; i >= 0; i--)
+        downHeapify(i, n, arr, isMax);
+
+    for (int i = n; i >= 0; i--)
     {
-        downHeapify(arr,i,isMax,n-1);
+        swap(arr[0], arr[n--]);
+        downHeapify(0, n, arr, isMax);
     }
 
-    int i = n-1;
-    while(i>=0){
-        swap(arr[0],arr[i--]);
-        downHeapify(arr,0,isMax,i);
-    }
-    
-    for(int ele: arr) cout<<ele<<" ";
-    cout<<endl;
+    for (int d : arr)
+        cout << d << " ";
 }
-
 int main()
 {
-    solve();
+    heapSort();
     return 0;
 }
