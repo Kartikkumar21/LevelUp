813a8cb4911db223d3aded87aa2f57a0cf1a8e29
Kartikkumar21 <60642198+Kartikkumar21@users.noreply.github.com>
2021-01-27 16:30:24 +0530

    update

--- cpp/lec16LLQues/que.cpp
+++ cpp/lec16LLQues/que.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
 struct ListNode
@@ -218,35 +220,41 @@ ListNode *orderList(ListNode *head)
     return th2;
 }
 
-//Leetcode 
+//Leetcode
 ListNode *mergeTwoLists(ListNode *l1, ListNode *l2)
 {
-    if(l1 == nullptr || l2 == nullptr) return (l1 == nullptr? l2:l1);
+    if (l1 == nullptr || l2 == nullptr)
+        return (l1 == nullptr ? l2 : l1);
 
-    ListNode* dummy=new ListNode(-1);
-    ListNode* prev = dummy;
+    ListNode *dummy = new ListNode(-1);
+    ListNode *prev = dummy;
 
-    ListNode* c1 = l1;
-    ListNode* c2 = l2;
-    while(c1!=nullptr && c2!=nullptr){
-        
-        if(c1->val < c2->val){
+    ListNode *c1 = l1;
+    ListNode *c2 = l2;
+    while (c1 != nullptr && c2 != nullptr)
+    {
+
+        if (c1->val < c2->val)
+        {
             prev->next = c1;
-            c1=c1->next;
-        }else{
+            c1 = c1->next;
+        }
+        else
+        {
             prev->next = c2;
-            c2=c2->next;
+            c2 = c2->next;
         }
 
         prev = prev->next;
     }
 
-    if(c1!=nullptr) prev->next = c1;
-    else prev->next = c2;
+    if (c1 != nullptr)
+        prev->next = c1;
+    else
+        prev->next = c2;
 
     return dummy->next;
 }
-
 
 //Leetcode 148
 ListNode *sortList(ListNode *head)
