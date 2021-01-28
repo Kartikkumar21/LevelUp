79480358b283b79f3623c786db5d9ef2c994c360
Kartikkumar21 <60642198+Kartikkumar21@users.noreply.github.com>
2021-01-28 21:03:00 +0530

    update

--- java/lec16Trees/AllNodeDistK.java
+++ java/lec16Trees/AllNodeDistK.java
@@ -40,9 +40,53 @@ class AllNodeDistK{
             }
             return null;
     } 
-    public static List<Integer> distanceK(TreeNode root, TreeNode target, int K){
-        
+    public static void kdown(Node node,Node block,int k,ArrayList<Integer> ans){
+        if(node==null || node == block || k < 0) return;
+
+        if(k==0){
+            ans.add(node.data);
+            return;
+        }
+
+        kdown(node.left,block,k-1,ans);
+        kdown(node.right,block,k-1,ans);
     }
+
+    public static void kFar(Node node,int data,int k){
+       ArrayList<Node> list = new ArrayList<>();
+       nodeToRootPath(node,data,list);
+
+       ArrayList<Inreger> ans = new ArrayList<>();
+       Node prev = null;
+       for(int i=0;i<list.size();i++){
+          kdown(list.get(i),prev,k-i,ans);
+          prev = list.get(i);
+       }
+    }
+
+    public static int kFar2(Node node,int data,int k,ArrayList<Integer> ans){
+        if(node == null) return -1;
+
+        if(node.data == data){
+            kdown(node,null,k,ans);
+            return 1;
+        }
+
+        int ld = kFar2(node.left, data, k, ans);
+        if(ld != -1){
+            kdown(node,node.left,k-ld,ans);
+            return ld + 1;
+        }
+
+        int rd = kFar2(node.right, data, k, ans);
+        if(rd != -1){
+            kdown(node,node.right,k-rd,ans);
+            return rd + 1;
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args){
         int[] arr={10,20,40,-1,-1,50,80,-1,-1,90,-1,-1,30,60,100,-1,-1,-1,70,110,-1,-1,120,-1,-1};
         Node root=constTree(arr);
